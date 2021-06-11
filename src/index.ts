835744e02381bc20bcf01a5003498fd06d9a0dc9
yanghuanrong <1176989241@qq.com>
2021-06-11 15:47:54 +0800

    feat: es5

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,32 @@
-import md5 from './utils/md5.js';
+import md5 from './utils/md5';
 import { randomString } from './utils/index';
+import Web from './adapter/web';
 
-const id = '39ee83f92ff3a195130596a4eaec5ddf';
-const key = 'a1223fca87f5d229953817f5c2493446';
+const secretKey = 'c8bed465c9e6a524';
+const securityCode = '999999';
 
-interface headerDTO {
-  readonly 'content-type': string;
-  readonly 'X-Bmob-SDK-Type': string;
-  readonly 'X-Bmob-Safe-Sign': string;
-  readonly 'X-Bmob-Safe-Timestamp': number;
-  readonly 'X-Bmob-Noncestr-Key': string;
-  readonly 'X-Bmob-Secret-Key': string;
-}
-
-function setHeader(route: string, method: string, parma: object): headerDTO {
+function setHeader(route: string, method: string, parma: object) {
   const t = Math.round(new Date().getTime() / 1000);
   const body =
     method === 'get' || method === 'delete' ? '' : JSON.stringify(parma);
   const random = randomString();
-  const sign = md5.utf8MD5(route + t + key + random + body);
-  const header: headerDTO = {
+  const sign = md5.utf8MD5(route + t + securityCode + random + body);
+  const header = {
     'content-type': 'application/json',
     'X-Bmob-SDK-Type': 'wechatApp',
     'X-Bmob-Safe-Sign': sign,
     'X-Bmob-Safe-Timestamp': t,
     'X-Bmob-Noncestr-Key': random,
-    'X-Bmob-Secret-Key': id,
+    'X-Bmob-Secret-Key': secretKey,
   };
   return header;
 }
 
-const route = `/1/classes/diary/fdffb09659`;
-const ajax = new XMLHttpRequest();
-ajax.open('GET', 'https://api2.bmob.cn/1/classes/diary/fdffb09659', true);
-
-const header = setHeader(route, 'GET', {});
-console.log(header);
-Object.keys(header).forEach((key) => {
-  const value = header[key];
-  ajax.setRequestHeader(key, value + '');
+const route = `/1/classes/_User`;
+const mehods = 'GET';
+const Serve = new Web({
+  baseURL: 'https://api2.bmob.cn',
+  headers: setHeader(route, 'get', {}),
 });
-ajax.send();
-ajax.onreadystatechange = function () {
-  if (ajax.readyState == 4 && ajax.status == 200) {
-    console.log(ajax.responseText);
-  }
-};
 
-console.log(setHeader);
+Serve.request(mehods, route);
